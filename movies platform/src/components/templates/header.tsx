5763cd600e6747dcaa995b13c2b86db312c6aace
armandonum <armandonunez404@gmail.com>
2024-08-13 21:45:34 -0400

    added  header ,templates

---
+++ movies platform/src/components/templates/header.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import '../../styles/header.css';
+
+type HeaderProps = {
+    title: string;
+    links: { text: string; href: string }[];
+};
+
+function Header({ title, links }: HeaderProps) {
+    return (
+        <header>
+            <h1>{title}</h1>
+            <nav>
+                <ul className="horizontal-menu">
+                    {links.map((link) => (
+                        <li key={link.href} className="horizontal-menu-item">
+                            <Link to={link.href}>{link.text}</Link >
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ movies platform/src/components/templates/template.tsx
@@ -0,0 +1,41 @@
+import Header from "./header";
+
+type Props= {
+    children: React.ReactNode;
+};
+
+
+const routes =[
+    {
+        href: '/',
+        text: 'Home',
+    },
+    {
+        href: '/newMovie',
+        text: 'NewMovie',
+    },
+    {
+        href: '/Genere',
+        text: 'Genere',
+    },
+    {
+        href: '/TVSeries',
+        text: 'TVSeries',
+    },
+   
+];
+
+function Template({ children }: Props) {
+    return (
+        <div>
+            <Header title="" links={routes} />
+            {children}
+            <footer>
+            
+            &copy; { new Date().getFullYear() }
+        </footer>
+        </div>
+    );
+}
+
+export default Template;
