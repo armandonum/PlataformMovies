a1a09a7fe47204c69fbf3167f97f6cce544a0837
armando <armandonunez404@gmail.com>
2024-08-15 12:31:31 -0400

    correction  some bugs

--- movies platform/src/components/pages/newMovie/NewMovie.tsx
+++ movies platform/src/components/pages/newMovie/NewMovie.tsx
@@ -1,18 +1,28 @@
 import MovieList from '../../../context/MovieList';
-import './Movie.css'; // Importa los estilos desde el archivo CSS
-
+import './Movie.css'; 
+import UpcomingMoviesList from '../newMovie/MovieList/UpcomingMoviesList';
+import CurrentlyPlayingMoviesList from './MovieList/CurrentlyPlayingMoviesList';
 export const Movie = () => {
   return (
     <div className="movie-list-container">
       <MovieList />
 
       <div className="coming-soon-section">
+        <h2>urrently playing</h2>
+        <div className="coming-soon-list">
+          {/* Aquí puedes agregar una lista de películas que están por venir */}
+          <div className="coming-soon-item">
+            
+          <CurrentlyPlayingMoviesList />
+          </div>
+        </div>
+      </div>
+      <div className="coming-soon-section">
         <h2>Coming Soon</h2>
         <div className="coming-soon-list">
           {/* Aquí puedes agregar una lista de películas que están por venir */}
           <div className="coming-soon-item">
-            <img src="https://via.placeholder.com/150" alt="Upcoming Movie" />
-            <p>Upcoming Movie 1</p>
+          <UpcomingMoviesList />
           </div>
         </div>
       </div>
